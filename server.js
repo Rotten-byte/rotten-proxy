025c1f4a6115206f0c32d330f4ed06852d54cddd
Rotten-byte <iimushroom503@gmail.com>
2026-08-21 14:19:06 -0600

    server.js

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ function getApiKey() {
  process.env.TIKTOOL_API_KEY2,
  process.env.TIKTOOL_API_KEY3,
  process.env.TIKTOOL_API_KEY4,
- process.env.TIKTOOL_API_KEY5
+ process.env.TIKTOOL_API_KEY5,
+ process.env.TIKTOOL_API_KEY6
+  
  ].filter(k => k && k.trim().length > 10);
  return keys.length === 0 ? null : keys[Math.floor(Math.random() * keys.length)].trim();
 }
